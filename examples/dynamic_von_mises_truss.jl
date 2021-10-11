45f151eeb67191736700581547b15b60b4265fa3
jorgepz <jorgepz@fing.edu.uy>
2021-10-11 15:16:07 -0300

    adding diagram

--- examples/dynamic_von_mises_truss.jl
+++ examples/dynamic_von_mises_truss.jl
@@ -1,1 +1,8 @@
 # # Dynamic Von Mises Truss
+
+#md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/models/dynamic_von_mises_truss.ipynb)
+#
+
+# ![](diagramDynVonMises.png)
+
+using StructuralDynamicsODESolvers, Plots
